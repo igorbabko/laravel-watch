b959d0f66218ec67661afd652692507cdf3f78db
Igor Babko <i.i.babko@gmail.com>
2024-07-08 12:39:16 +0300

    .

--- resources/views/components/footer/newsletter.blade.php
+++ resources/views/components/footer/newsletter.blade.php
@@ -1,7 +1,7 @@
 <div class="flex justify-center gap-8">
     <h3 class="text-3xl font-bold">Join the newsletter</h3>
     <form action="/" class="flex gap-2">
-        <input class="border-2 p-2 rounded" type="email" placeholder="Your email">
+        <input class="border-2 p-2 rounded w-80" type="email" placeholder="Your email">
         <button class="bg-purple-500 p-2 text-white rounded px-4">Join</button>
     </form>
 </div>
