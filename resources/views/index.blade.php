a7b4606f5fe5176737f6be0dfe87d090d0ae7c65
Igor Babko <i.i.babko@gmail.com>
2024-07-09 14:51:54 +0300

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,25 @@
     <x-hero/>
     <section class="px-2">
         <div class="container py-20 mx-auto flex flex-col items-center gap-6">
+            <h2 class="text-5xl font-black">Topics</h2>
+            <hr class="border-4 border-pink-500 my-4 w-20 rounded-lg">
+            <div class="flex gap-4">
+                <div>
+                    <div>
+                        <img src="/" alt="">
+                        <span>Laravel</span>
+                    </div>
+                    <hr>
+                    <div>
+                        <p><span class="font-semibold">4</span> courses</p>
+                        <p><span class="font-semibold">72</span> lessons</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="px-2">
+        <div class="container py-20 mx-auto flex flex-col items-center gap-6">
             <h2 class="text-5xl font-black">Latest Courses</h2>
             <hr class="border-4 border-pink-500 my-4 w-20 rounded-lg">
             <div class="flex gap-4">
@@ -22,5 +41,5 @@
             </div>
         </div>
     </section>
-    <x-footer.newsletter/>
+    <x-newsletter/>
 </x-layout>
