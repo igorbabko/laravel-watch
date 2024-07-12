006612b1b62807055d7612cd312b99198cbe119a
Igor Babko <i.i.babko@gmail.com>
2024-07-12 14:34:48 +0300

    .

--- resources/views/components/header/logo.blade.php
+++ resources/views/components/header/logo.blade.php
@@ -2,7 +2,7 @@
 
 <a
     href="{{ route("index") }}"
-    class="flex items-center gap-1 text-3xl font-bold"
+    class="flex items-center gap-1 rounded-full text-3xl font-bold"
 >
     <x-icon-circle-play class="size-16" />
     {{ $withName ? "Watch" : "" }}
--- resources/views/components/topic.blade.php
+++ resources/views/components/topic.blade.php
@@ -1,13 +1,27 @@
-@props(['name', 'logo', 'courseCount', 'lessonCount'])
+@props([
+    "name",
+    "logo",
+    "courseCount",
+    "lessonCount",
+])
 
-<div class="bg-gray-100 p-4 rounded-xl flex flex-col items-center gap-2">
+<a
+    href="/courses"
+    class="flex flex-col items-center gap-2 rounded-xl bg-gray-100 p-4 hover:bg-gray-200"
+>
     <div class="flex items-center gap-4">
-        <img src="{{ $logo }}" alt="" class="size-12">
+        <img src="{{ $logo }}" alt="" class="size-12" />
         <h3 class="text-4xl font-semibold">{{ $name }}</h3>
     </div>
-    {{--                    <hr class="border border-gray-200 w-full my-2">--}}
+    {{-- <hr class="border border-gray-200 w-full my-2"> --}}
     <div class="text-center">
-        <p><span class="font-semibold">{{ $courseCount }}</span> courses</p>
-        <p><span class="font-semibold">{{ $lessonCount }}</span> lessons</p>
+        <p>
+            <span class="font-semibold">{{ $courseCount }}</span>
+            courses
+        </p>
+        <p>
+            <span class="font-semibold">{{ $lessonCount }}</span>
+            lessons
+        </p>
     </div>
-</div>
+</a>
