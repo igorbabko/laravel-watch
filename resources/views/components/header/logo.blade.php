515ea89db90f0dab4af6c1abfb0e2a2482782a32
Igor Babko <i.i.babko@gmail.com>
2024-07-13 12:26:40 +0300

    .

--- resources/views/components/header/logo.blade.php
+++ resources/views/components/header/logo.blade.php
@@ -1,8 +1,7 @@
 @props(["withName" => false])
 
 <a
-    href="{{ route("index") }}"
-    {{ $attributes->merge(["class" => "flex items-center gap-2 text-3xl font-bold"]) }}
+    {{ $attributes->merge(["href" => route("index"), "class" => "flex items-center gap-2 text-3xl font-bold"]) }}
 >
     @if ($withName)
         <x-icon-circle-play-2 class="size-10" />
