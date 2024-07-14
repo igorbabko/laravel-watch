a288da802fdc91fa74e0aea496bd9d3015cd7a39
Igor Babko <i.i.babko@gmail.com>
2024-07-14 12:46:37 +0300

    .

--- resources/views/components/courses.blade.php
+++ resources/views/components/courses.blade.php
@@ -1,7 +1,5 @@
-@props(["count" => 3])
-
 <div class="grid w-full gap-4 sm:grid-cols-2 lg:grid-cols-3">
-    @foreach (range(1, $count) as $i)
+    @foreach (range(1, $count ?? 3) as $i)
         <x-course />
     @endforeach
 </div>
