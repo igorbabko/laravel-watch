e1204158a43ae737e3784e9bdb4b201a142d5cc5
Igor Babko <i.i.babko@gmail.com>
2024-07-14 17:31:19 +0300

    .

---
+++ resources/views/components/lessons.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-col gap-2">
+    @foreach (range(1, 23) as $i)
+        <x-lesson />
+    @endforeach
+</div>
--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,12 +1,11 @@
 <x-layouts.guest>
     <x-section>
-        <x-container class="flex flex-col gap-4 py-20">
+        <x-container class="flex flex-col gap-4 pb-10 pt-20 md:py-20">
             <x-heading>Course Title</x-heading>
-            <div class="flex items-start gap-6">
-                <div class="flex w-1/2 flex-col gap-2">
-                    {{-- <x-course.footer /> --}}
+            <div class="flex flex-col items-start gap-6 md:flex-row">
+                <div class="flex flex-col gap-2 md:w-1/2">
                     <img src="/img/full.jpg" alt="" class="rounded-lg shadow" />
-                    <div class="flex gap-2">
+                    <div class="flex flex-col gap-2 sm:flex-row">
                         <x-button-primary
                             href="{{ route('index') }}"
                             class="flex-grow"
@@ -22,7 +21,10 @@
                             Source Code
                         </x-button-secondary>
                     </div>
-                    <h3 class="mt-4 text-2xl font-bold">Overview</h3>
+                    <h3 class="mt-4 text-xl font-bold md:text-2xl">Overview</h3>
+                    <h3 class="text-2xl text-lg font-medium">
+                        Last Updated: May 14, 2024
+                    </h3>
                     <div>
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit. A alias aliquam aliquid aperiam asperiores
@@ -38,36 +40,13 @@
                         quibusdam!
                     </div>
                 </div>
-                <div class="flex w-1/2 flex-col gap-4">
-                    {{-- <x-course.footer /> --}}
-
-                    <div class="text-xl font-bold">
+                <div class="flex flex-col gap-4 md:w-1/2">
+                    <div class="text-xl font-bold md:text-2xl">
                         <span>25 lessons</span>
                         &middot;
                         <span>2h 25m</span>
                     </div>
-                    <div class="flex flex-col gap-2">
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                        <x-lesson />
-                    </div>
+                    <x-lessons />
                 </div>
                 {{-- <div> --}}
                 {{-- <div class="flex items-center justify-between"> --}}
