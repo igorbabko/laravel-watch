92bb01ee7a596f41ab4640649570beb9d53e8abb
Igor Babko <i.i.babko@gmail.com>
2024-07-14 20:22:33 +0300

    .

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -31,21 +31,11 @@
 @elseif ($name === "circle-play")
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
+        viewBox="0 0 512 512"
         {{ $attributes }}
     >
         <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-        />
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
+            d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c7.6-4.2 16.8-4.1 24.3 .5l144 88c7.1 4.4 11.5 12.1 11.5 20.5s-4.4 16.1-11.5 20.5l-144 88c-7.4 4.5-16.7 4.7-24.3 .5s-12.3-12.2-12.3-20.9V168c0-8.7 4.7-16.7 12.3-20.9z"
         />
     </svg>
 @elseif ($name === "circle-play-2")
