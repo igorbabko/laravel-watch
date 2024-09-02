315f35b6afe5ca3ca936f68f371dec0c73150d65
Igor Babko <i.i.babko@gmail.com>
2024-09-02 09:42:58 +0300

    .

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,35 +1,19 @@
 @props(["type" => "primary", "size" => "md"])
 
+@php
+    $mergedAttributes = $attributes
+        ->class([
+            "bg-purple-500 text-white fill-white hover:bg-purple-600" => $type === "primary",
+            "bg-gray-100 hover:bg-gray-200" => $type === "secondary",
+            "px-3 py-1.5 text-sm" => $size === "sm",
+            "px-3 py-2 lg:px-4 lg:py-3 text-sm" => $size === "md",
+            "px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-lg" => $size === "lg",
+        ])
+        ->merge(["class" => "flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium"]);
+@endphp
+
 @if ($attributes->has("href"))
-    <a
-        {{
-            $attributes
-                ->class([
-                    "bg-purple-500 text-white fill-white hover:bg-purple-600" => $type === "primary",
-                    "bg-gray-100 hover:bg-gray-200" => $type === "secondary",
-                    "px-3 py-1.5 text-sm" => $size === "sm",
-                    "px-4 py-3" => $size === "md",
-                    "px-4 py-2 lg:px-6 lg:py-4 text-sm lg:text-lg" => $size === "lg",
-                ])
-                ->merge(["class" => "flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium"])
-        }}
-    >
-        {{ $slot }}
-    </a>
+    <a {{ $mergedAttributes }}>{{ $slot }}</a>
 @else
-    <button
-        {{
-            $attributes
-                ->class([
-                    "bg-purple-500 text-white fill-white hover:bg-purple-600" => $type === "primary",
-                    "bg-gray-100 hover:bg-gray-200" => $type === "secondary",
-                    "px-3 py-1.5 text-sm" => $size === "sm",
-                    "px-4 py-3" => $size === "md",
-                    "px-6 py-4 text-lg" => $size === "lg",
-                ])
-                ->merge(["class" => "flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium"])
-        }}
-    >
-        {{ $slot }}
-    </button>
+    <button {{ $mergedAttributes }}>{{ $slot }}</button>
 @endif
