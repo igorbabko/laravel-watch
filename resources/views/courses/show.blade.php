6873d72984153f21744a554d79410ff48299242f
Igor Babko <i.i.babko@gmail.com>
2024-09-03 08:38:44 +0300

    .

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,23 +1,27 @@
 <x-layouts.guest>
     <x-section>
-        <x-container class="flex flex-col gap-4 pb-10 pt-20 md:py-20">
-            <x-page-heading>{{ $course->title }}</x-page-heading>
-            <div class="flex flex-col gap-6 md:flex-row">
-                <div class="md:w-1/2">
-                    <x-course-info
-                        :course="$course"
-                        class="md:sticky md:top-24"
-                    />
-                </div>
-                <div class="flex w-full flex-col gap-4 md:w-1/2">
-                    <div class="text-right text-xl font-medium">
-                        <span>{{ $course->lessons()->count() }} lessons</span>
-                        &middot;
-                        <span>2h 25m</span>
+        <x-container class="pb-10 pt-20 md:py-20">
+            <x-stack column>
+                <x-page-heading>{{ $course->title }}</x-page-heading>
+                <x-stack column gap="lg" class="md:flex-row">
+                    <div class="md:w-1/2">
+                        <x-course-info
+                            :course="$course"
+                            class="md:sticky md:top-24"
+                        />
                     </div>
-                    <x-lessons :lessons="$course->lessons" />
-                </div>
-            </div>
+                    <s-stack column class="w-full md:w-1/2">
+                        <div class="text-right text-xl font-medium">
+                            <span>
+                                {{ $course->lessons()->count() }} lessons
+                            </span>
+                            &middot;
+                            <span>2h 25m</span>
+                        </div>
+                        <x-lessons :lessons="$course->lessons" />
+                    </s-stack>
+                </x-stack>
+            </x-stack>
         </x-container>
     </x-section>
 </x-layouts.guest>
