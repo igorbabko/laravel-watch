922194481471b6710d156c281b2d3404ae356988
Igor Babko <i.i.babko@gmail.com>
2024-09-13 12:00:25 +0300

    .

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,8 +1,11 @@
 <x-stack
-    :attributes="$attributes->merge(['class' => 'text-3xl font-bold items-center'])"
-    :href="route('index')"
-    tag="a"
+    :attributes="$attributes->merge([
+        'class' => 'text-3xl font-bold',
+        'href' => route('index'),
+        'centered' => true,
+        'tag' => 'a',
+    ])"
 >
     <x-icon name="circle-play-2" size="xl" />
-    {{ $slot }}
+    <span class="hidden md:inline-block">Watch</span>
 </x-stack>
