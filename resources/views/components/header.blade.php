cfca6b6fa07885bb5ce3c058121177a889c22a47
Igor Babko <i.i.babko@gmail.com>
2024-09-13 18:20:53 +0300

    .

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,24 +1,14 @@
-@php
-    $items = [
-        "index" => "Home",
-        "courses.index" => "Courses",
-        "contact" => "Contact",
-        "login" => "Log In",
-        "register" => "Register",
-    ];
-@endphp
-
 <header
     {{ $attributes->merge(["class" => "fixed inset-x-0 z-10 bg-white border-b"]) }}
 >
     <x-section tag="header">
         <x-container centered class="hidden justify-between py-4 md:flex">
             <x-logo />
-            <x-nav :$items />
+            <x-nav />
         </x-container>
         <x-container class="justify-between py-2 md:hidden">
             <x-logo />
-            <x-nav :$items is-mobile />
+            <x-nav is-mobile />
         </x-container>
     </x-section>
 </header>
--- resources/views/components/nav/index.blade.php
+++ resources/views/components/nav/index.blade.php
@@ -4,6 +4,14 @@
 ])
 
 @php
+    $items = [
+        "index" => "Home",
+        "courses.index" => "Courses",
+        "contact" => "Contact",
+        "login" => "Log In",
+        "register" => "Register",
+    ];
+
     $attributes = $isMobile
         ? $attributes->merge([
             "class" => "peer absolute inset-x-0 top-0 hidden bg-white pt-2 pb-4 px-4 mt-[70px] target:flex border-b",
