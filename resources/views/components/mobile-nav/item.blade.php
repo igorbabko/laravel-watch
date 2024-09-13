a904ef8589c18b1bb042a547af16de4348632a69
Igor Babko <i.i.babko@gmail.com>
2024-09-13 10:17:45 +0300

    .

--- resources/views/components/mobile-nav/item.blade.php
+++ resources/views/components/mobile-nav/item.blade.php
@@ -1,13 +1,15 @@
 @props(["isActive" => false])
 
 @php
-    $attributes = $attributes
-        ->class(["bg-gray-100" => $isActive])
-        ->merge(["class" => "inline-block w-full rounded-md p-4 text-lg hover:bg-gray-100"]);
+    $attributes = $attributes->class(["bg-gray-100" => $isActive])->merge([
+        "class" => "inline-block w-full rounded-md p-4 text-lg hover:bg-gray-100",
+        "weight" => "semibold",
+        "tag" => "a",
+    ]);
 @endphp
 
 <li>
-    <x-text tag="a" weight="semibold" :$attributes>
+    <x-text :$attributes>
         {{ $slot }}
     </x-text>
 </li>
