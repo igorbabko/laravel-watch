43a1ce6cdbf0be7425a988542219c321a3ff72f3
Igor Babko <i.i.babko@gmail.com>
2024-09-14 09:18:01 +0300

    .

--- resources/views/components/lesson.blade.php
+++ resources/views/components/lesson.blade.php
@@ -1,19 +1,19 @@
 <x-card
-    :attributes="$attributes->merge(['class' => 'bg-gray-100 hover:bg-gray-200'])"
+    :attributes="$attributes->merge([
+        'class' => 'bg-gray-100 hover:bg-gray-200',
+        'href' => route('lessons.show', $lesson),
+        'tag' => 'a',
+    ])"
 >
-    <a href="{{ route("lessons.show", $lesson) }}">
-        <x-stack column class="p-4">
-            <x-stack class="justify-between font-mono">
-                <x-text tag="span" size="sm">
-                    {{ Str::padLeft($lesson->number, 2, 0) }}
-                </x-text>
-                <x-text tag="span" size="sm" class="text-gray-600">
-                    5m 32s
-                </x-text>
-            </x-stack>
-            <x-heading tag="h3" class="text-base">
-                {{ $lesson->title }}
-            </x-heading>
+    <x-stack column class="p-4">
+        <x-stack class="justify-between font-mono">
+            <x-text tag="span" size="sm">
+                {{ Str::padLeft($lesson->number, 2, 0) }}
+            </x-text>
+            <x-text tag="span" size="sm" class="text-gray-600">5m 32s</x-text>
         </x-stack>
-    </a>
+        <x-heading tag="h3" class="text-base">
+            {{ $lesson->title }}
+        </x-heading>
+    </x-stack>
 </x-card>
