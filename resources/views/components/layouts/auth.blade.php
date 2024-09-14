e2f2362c46ecb6b934100e276a2fd1ff79e4317a
Igor Babko <i.i.babko@gmail.com>
2024-09-14 09:16:11 +0300

    .

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -7,19 +7,19 @@
             content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
         />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>Watch</title>
+        <title>{{ config("app.name") }}</title>
         @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
     <x-stack
-        column
-        centered
         class="min-h-screen justify-center gap-6 p-4 sm:px-8 sm:py-6"
+        centered
+        column
     >
         <header>
             <x-logo class="overflow-hidden rounded-full" />
         </header>
-        <main class="w-full max-w-96 md:w-2/3 xl:w-1/3">
-            <x-card>{{ $slot }}</x-card>
-        </main>
+        <x-card tag="main" class="w-full max-w-96 md:w-2/3 xl:w-1/3">
+            {{ $slot }}
+        </x-card>
     </x-stack>
 </html>
