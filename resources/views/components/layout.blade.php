512d72086355f12f72c855f9cfdd3cb07ad81cf4
Igor Babko <i.i.babko@gmail.com>
2024-10-02 07:21:35 +0300

    - header components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,52 +9,7 @@
         <title>{{ config('app.name') }}</title>
     </head>
     <body>
-        <header class="fixed inset-x-0 bg-white border-b">
-            <div class="container flex items-center justify-between py-2 px-4">
-                <a class="flex items-center gap-1 text-3xl font-bold" href="{{ route('index') }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-12">
-                        <path fill-rule="evenodd"
-                              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
-                              clip-rule="evenodd"/>
-                    </svg>
-                    {{ config('app.name') }}
-                </a>
-                <nav>
-                    <ul class="flex gap-8">
-                        <li>
-                            <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="{{ route('index') }}">
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="{{ route('courses') }}">
-                                Courses
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="{{ route('contact') }}">
-                                Contact
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="{{ route('login') }}">
-                                Login
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="{{ route('register') }}">
-                                Register
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+        <x-header />
         <main class="h-screen grid place-items-center text-7xl font-black">
             {{ $slot }}
         </main>
