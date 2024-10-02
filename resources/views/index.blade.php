775ab75556923552ee48e78d5e7f87e48c4a0b53
Igor Babko <i.i.babko@gmail.com>
2024-10-02 06:40:42 +0300

    - nav links

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
     <body>
         <header class="fixed inset-x-0 bg-white border-b">
             <div class="container flex items-center justify-between p-4">
-                <a class="flex items-center gap-1 text-3xl font-bold" href="{{ config('app.url') }}">
+                <a class="flex items-center gap-1 text-3xl font-bold" href="{{ route('index') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-12">
                         <path fill-rule="evenodd"
                               d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
@@ -23,31 +23,31 @@
                     <ul class="flex gap-8">
                         <li>
                             <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="{{ config('app.url') }}">
+                               href="{{ route('index') }}">
                                 Home
                             </a>
                         </li>
                         <li>
                             <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="https://laravel-watch.test/courses">
+                               href="{{ route('courses') }}">
                                 Courses
                             </a>
                         </li>
                         <li>
                             <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="https://laravel-watch.test/contact">
+                               href="{{ route('contact') }}">
                                 Contact
                             </a>
                         </li>
                         <li>
                             <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="https://laravel-watch.test/login">
+                               href="{{ route('login') }}">
                                 Log In
                             </a>
                         </li>
                         <li>
                             <a class="text-xl font-semibold hover:text-indigo-600"
-                               href="https://laravel-watch.test/register">
+                               href="{{ route('register') }}">
                                 Register
                             </a>
                         </li>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
+Route::view('/', 'index')->name('index');
+Route::get('/courses', fn () => 'Courses')->name('courses');
+Route::get('/contact', fn () => 'Contact')->name('contact');
+Route::get('/login', fn () => 'Login')->name('login');
+Route::get('/register', fn () => 'Register')->name('register');
