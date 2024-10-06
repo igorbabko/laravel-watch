8b6084cbdd6baa31e1314dad52a21fc77fdd84e4
Igor Babko <i.i.babko@gmail.com>
2024-10-06 12:27:16 +0300

    - course component

---
+++ resources/views/components/course.blade.php
@@ -0,0 +1,18 @@
+<a class="rounded-md bg-gray-100 hover:bg-gray-200" href="{{ route('course') }}">
+    <article class="flex flex-col gap-1 p-4">
+        <header>
+            <h3 class="font-semibold text-xl">{{ $course['title'] }}</h3>
+        </header>
+        <p class="font-normal mb-8 mt-2">{{ $course['description'] }}</p>
+        <footer class="flex gap-2 justify-between">
+            <div class="flex items-center gap-2">
+                <x-icon name="circle-play" class="size-4"/>
+                <span class="text-sm font-semibold">{{ $course['lessons_count'] }} lessons</span>
+            </div>
+            <div class="flex items-center gap-2">
+                <x-icon name="clock" class="size-4"/>
+                <span class="text-sm font-semibold">{{ $course['length'] }}</span>
+            </div>
+        </footer>
+    </article>
+</a>
--- resources/views/components/courses.blade.php
+++ resources/views/components/courses.blade.php
@@ -1,29 +1,38 @@
+@php
+    $courses = [
+        [
+            'title' => 'Course 1',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis dicta incidunt maiores odio odit officiis quidem quisquam sit ullam.',
+            'lessons_count' => 21,
+            'length' => '2h 30min',
+        ],
+        [
+            'title' => 'Course 2',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis dicta incidunt maiores odio odit officiis quidem quisquam sit ullam.',
+            'lessons_count' => 16,
+            'length' => '1h 40min',
+        ],
+        [
+            'title' => 'Course 3',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis dicta incidunt maiores odio odit officiis quidem quisquam sit ullam.',
+            'lessons_count' => 8,
+            'length' => '45min',
+        ],
+        [
+            'title' => 'Course 4',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis dicta incidunt maiores odio odit officiis quidem quisquam sit ullam.',
+            'lessons_count' => 14,
+            'length' => '1h 20min',
+        ]
+    ];
+@endphp
+
 <section class="px-10">
     <div class="flex flex-col gap-8 container pb-24">
         <h2 class="text-center font-bold text-4xl">Latest Courses</h2>
         <div class="grid gap-4 grid-cols-2">
-            @foreach (range(0, 3) as $i)
-                <a class="rounded-md bg-gray-100 hover:bg-gray-200"
-                   href="{{ route('course') }}">
-                    <article class="flex flex-col gap-1 p-4">
-                        <header>
-                            <h3 class="font-semibold text-xl">Repellendus maxime</h3>
-                        </header>
-                        <p class="font-normal mb-8 mt-2">
-                            Eligendi aliquam modi magnam voluptatem hic quisquam. Soluta sapiente qui iusto aperiam.
-                        </p>
-                        <footer class="flex gap-2 justify-between">
-                            <div class="flex items-center gap-2">
-                                <x-icon name="circle-play" class="size-4"/>
-                                <span class="text-sm font-semibold">21 lessons</span>
-                            </div>
-                            <div class="flex items-center gap-2">
-                                <x-icon name="clock" class="size-4"/>
-                                <span class="text-sm font-semibold">2h 30m</span>
-                            </div>
-                        </footer>
-                    </article>
-                </a>
+            @foreach ($courses as $course)
+                <x-course :$course/>
             @endforeach
         </div>
     </div>
