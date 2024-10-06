9a970eceda38260cf64df031ac7e7180f2d07415
Igor Babko <i.i.babko@gmail.com>
2024-10-06 09:35:30 +0300

    - move nav items and socials to config

--- resources/views/components/footer/nav.blade.php
+++ resources/views/components/footer/nav.blade.php
@@ -1,7 +1,11 @@
-<nav>
-    <ul class="flex gap-4">
-        @foreach ($items as $routeName => $label)
-            <x-footer.nav-item :href="route($routeName)">{{ $label }}</x-footer.nav-item>
-        @endforeach
-    </ul>
-</nav>
+@php
+    $items = collect(config('watch.nav_items'))->reject(
+        fn ($label, $routeName) => in_array($routeName, ['login', 'register'])
+    );
+@endphp
+
+<ul class="flex gap-4">
+    @foreach ($items as $routeName => $label)
+        <x-footer.nav-item :href="route($routeName)">{{ $label }}</x-footer.nav-item>
+    @endforeach
+</ul>
