9a52ea0ab4a0d5ccbbbba3a929b2b33fa2804495
Igor Babko <i.i.babko@gmail.com>
2024-10-06 11:43:58 +0300

    hero

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,22 @@
+<section>
+    <div class="container flex flex-col justify-center items-center gap-2 min-h-screen py-40">
+        <div>
+            <h1 class="font-extrabold text-5xl">Learn Web Development</h1>
+        </div>
+        <div>
+                <span class="text-xl font-medium">
+                    High-quality video tutorials to master web development.
+                </span>
+        </div>
+        <div class="flex gap-2 mt-6">
+            <a class="rounded-md font-semibold px-4 py-2 bg-violet-500 text-white hover:bg-violet-600"
+               href="{{ route('register') }}">
+                Get Started
+            </a>
+            <a class="rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200"
+               href="{{ route('courses') }}">
+                Explore Courses
+            </a>
+        </div>
+    </div>
+</section>
