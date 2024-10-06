8d42c040b5b98f18cb7dae2865bb1848dcd74d08
Igor Babko <i.i.babko@gmail.com>
2024-10-06 10:37:44 +0300

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,20 @@
 <x-layout>
-    <section class="relative px-10">
-        <div class="flex flex-col justify-center items-center gap-2 container min-h-screen py-40">
-            <div class="flex flex-col gap-2">
-                <h1 class="text-center font-extrabold text-5xl">
-                    Learn Web Development
-                </h1>
+    <section>
+        <div class="container flex flex-col justify-center items-center gap-2 min-h-screen py-40">
+            <div>
+                <h1 class="font-extrabold text-5xl">Learn Web Development</h1>
             </div>
-            <p class="text-center text-xl font-medium">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, quis.
-            </p>
+            <div>
+                <span class="text-xl font-medium">
+                    High-quality video tutorials to master web development.
+                </span>
+            </div>
             <div class="flex gap-2 mt-6">
-                <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-violet-500 text-white hover:bg-violet-600"
+                <a class="rounded-md font-semibold px-4 py-2 bg-violet-500 text-white hover:bg-violet-600"
                    href="{{ route('register') }}">
                     Get Started
                 </a>
-                <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200"
+                <a class="rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200"
                    href="{{ route('courses') }}">
                     Explore Courses
                 </a>
