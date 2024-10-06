48f0c9aecb6734b377d4378f7c3949fbd5059f28
Igor Babko <i.i.babko@gmail.com>
2024-10-06 11:44:21 +0300

    .

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -4,9 +4,7 @@
             <h1 class="font-extrabold text-5xl">Learn Web Development</h1>
         </div>
         <div>
-                <span class="text-xl font-medium">
-                    High-quality video tutorials to master web development.
-                </span>
+            <span class="text-xl font-medium">High-quality video tutorials to master web development.</span>
         </div>
         <div class="flex gap-2 mt-6">
             <a class="rounded-md font-semibold px-4 py-2 bg-violet-500 text-white hover:bg-violet-600"
