b8861af7f0cd1aca86d036837c69440d8ffcee19
Igor Babko <i.i.babko@gmail.com>
2024-10-07 13:29:42 +0300

    04. Tailwind CSS

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Watch</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold underline text-violet-600">
+            Hello world!
+        </h1>
+    </body>
+</html>
