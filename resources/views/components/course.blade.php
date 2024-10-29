e20085444e9305e042a6e316cd21dd7a7ccd6990
Igor Babko <i.i.babko@gmail.com>
2024-10-29 06:46:57 +0200

    22. Fetching courses from DB

--- resources/views/components/course.blade.php
+++ resources/views/components/course.blade.php
@@ -1,17 +1,17 @@
-<a href="{{ route('course', $course['id']) }}" class="rounded-md bg-gray-100 hover:bg-gray-200">
+<a href="{{ route('course', $course->id) }}" class="rounded-md bg-gray-100 hover:bg-gray-200">
     <article class="p-4">
         <header>
-            <h3 class="font-semibold text-xl">{{ $course['title'] }}</h3>
+            <h3 class="font-semibold text-xl">{{ $course->title }}</h3>
         </header>
-        <p class="font-normal mb-8 mt-2">{{ $course['description'] }}</p>
+        <p class="font-normal mb-8 mt-2">{{ $course->description }}</p>
         <footer class="flex gap-2 justify-between">
             <div class="flex items-center gap-2">
                 <x-icon name="film" class="size-4"/>
-                <span class="text-sm font-semibold">{{ $course['lessons_count'] }} lessons</span>
+                <span class="text-sm font-semibold">{{ $course->lessons_count }} lessons</span>
             </div>
             <div class="flex items-center gap-2">
                 <x-icon name="clock" class="size-4"/>
-                <span class="text-sm font-semibold">{{ $course['length'] }}</span>
+                <span class="text-sm font-semibold">{{ $course->length }}</span>
             </div>
         </footer>
     </article>
--- routes/web.php
+++ routes/web.php
@@ -2,42 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-$courses = [
-    [
-        'id' => 0,
-        'title' => 'Course 1',
-        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque cumque ea fuga numquam, porro quam quas reprehenderit sint temporibus.',
-        'lessons_count' => 21,
-        'length' => '2h 30min',
-    ],
-    [
-        'id' => 1,
-        'title' => 'Course 2',
-        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque cumque ea fuga numquam, porro quam quas reprehenderit sint temporibus.',
-        'lessons_count' => 16,
-        'length' => '1h 40min',
-    ],
-    [
-        'id' => 2,
-        'title' => 'Course 3',
-        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque cumque ea fuga numquam, porro quam quas reprehenderit sint temporibus.',
-        'lessons_count' => 8,
-        'length' => '45min',
-    ],
-    [
-        'id' => 3,
-        'title' => 'Course 4',
-        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque cumque ea fuga numquam, porro quam quas reprehenderit sint temporibus.',
-        'lessons_count' => 14,
-        'length' => '1h 20min',
-    ],
-];
+Route::get('/', function () {
+    $courses = DB::select('select * from courses');
 
-Route::get('/', fn () => view('index', ['courses' => $courses]))->name('index');
+    return view('index', compact('courses'));
+})->name('index');
 
-Route::get('/courses', fn () => view('courses', ['courses' => $courses]))->name('courses');
+Route::get('/courses', function () {
+    $courses = DB::select('select * from courses');
 
-Route::get('/course/{id}', fn ($id) => view('course', ['course' => $courses[$id]]))->name('course');
+    return view('courses', compact('courses'));
+})->name('courses');
+
+Route::get('/course/{id}', function ($id) {
+    $course = DB::selectOne('select * from courses where id =:id', compact('id'));
+
+    return view('course', compact('course'));
+})->name('course');
 
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/login', 'login')->name('login');
