3b28a055efc93f53e58350bea9fa882a4862e7bc
Igor Babko <i.i.babko@gmail.com>
2024-10-29 06:19:51 +0200

    21. Showing course by ID

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,28 @@ use Illuminate\Support\Facades\Route;
 
 $courses = [
     [
+        'id' => 0,
         'title' => 'Course 1',
         'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque cumque ea fuga numquam, porro quam quas reprehenderit sint temporibus.',
         'lessons_count' => 21,
         'length' => '2h 30min',
     ],
     [
+        'id' => 1,
         'title' => 'Course 2',
         'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque cumque ea fuga numquam, porro quam quas reprehenderit sint temporibus.',
         'lessons_count' => 16,
         'length' => '1h 40min',
     ],
     [
+        'id' => 2,
         'title' => 'Course 3',
         'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque cumque ea fuga numquam, porro quam quas reprehenderit sint temporibus.',
         'lessons_count' => 8,
         'length' => '45min',
     ],
     [
+        'id' => 3,
         'title' => 'Course 4',
         'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque cumque ea fuga numquam, porro quam quas reprehenderit sint temporibus.',
         'lessons_count' => 14,
@@ -33,7 +37,7 @@ Route::get('/', fn () => view('index', ['courses' => $courses]))->name('index');
 
 Route::get('/courses', fn () => view('courses', ['courses' => $courses]))->name('courses');
 
-Route::get('/course', fn () => view('course', ['course' => $courses[0]]))->name('course');
+Route::get('/course/{id}', fn ($id) => view('course', ['course' => $courses[$id]]))->name('course');
 
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/login', 'login')->name('login');
