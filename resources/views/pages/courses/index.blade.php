4cf52432c95104173a56154835a646a36a5ec4c8
Igor Babko <i.i.babko@gmail.com>
2024-11-11 09:55:34 +0200

    36. Course search component

--- resources/views/pages/courses/index.blade.php
+++ resources/views/pages/courses/index.blade.php
@@ -1,5 +1,14 @@
 <x-layout>
-    <div class="pt-32">
-        <x-courses heading="Courses" :$courses/>
-    </div>
+    <section class="pt-32 px-10">
+        <div class="container flex flex-col gap-8 pb-24">
+            <h1 class="text-center font-extrabold text-5xl">Courses</h1>
+            <x-course-search/>
+            <div class="grid grid-cols-2 gap-4">
+                @foreach ($courses as $course)
+                    <x-course :$course/>
+                @endforeach
+            </div>
+            {{ $courses->links() }}
+        </div>
+    </section>
 </x-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,7 @@ export default {
             center: true,
         }
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
