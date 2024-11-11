be88198243929a2fd966ccf6bf52c1f12856803d
Igor Babko <i.i.babko@gmail.com>
2024-11-11 08:04:06 +0200

    32. Auto factory generation

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -8,11 +8,14 @@ class CourseFactory extends Factory
 {
     public function definition(): array
     {
+        $hours = $this->faker->numberBetween(1, 15);
+        $minutes = $this->faker->numberBetween(1, 59);
+
         return [
-            'title' => $this->faker->words(rand(2, 6), true),
-            'description' => $this->faker->text(),
-            'lessons_count' => $this->faker->numberBetween(10, 50),
-            'length' => "{$this->faker->numberBetween(1, 15)}h {$this->faker->numberBetween(1, 59)}min",
+            'title' => ucfirst(fake()->words(rand(2, 6), true)),
+            'description' => fake()->paragraph(2),
+            'lessons_count' => fake()->numberBetween(10, 50),
+            'length' => "{$hours}h {$minutes}min",
         ];
     }
 }
--- resources/views/components/course.blade.php
+++ resources/views/components/course.blade.php
@@ -1,10 +1,10 @@
 <a href="{{ route('courses.show', $course->id) }}" class="rounded-md bg-gray-100 hover:bg-gray-200">
-    <article class="p-4">
+    <article class="flex flex-col p-4 h-full">
         <header>
             <h3 class="font-semibold text-xl">{{ $course->title }}</h3>
         </header>
         <p class="font-normal mb-8 mt-2">{{ $course->description }}</p>
-        <footer class="flex gap-2 justify-between">
+        <footer class="flex gap-2 justify-between mt-auto">
             <div class="flex items-center gap-2">
                 <x-icon name="film" class="size-4"/>
                 <span class="text-sm font-semibold">{{ $course->lessons_count }} lessons</span>
