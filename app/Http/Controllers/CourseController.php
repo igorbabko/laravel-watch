b8d7840acde2f644b379df1593a29142d8fe9295
Igor Babko <i.i.babko@gmail.com>
2024-11-11 10:23:27 +0200

    37. Course search

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CourseController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
-        $courses = Course::latest()->paginate();
+        $courses = Course::latest()
+            ->whereLike('title', "%{$request->search}%")
+            ->orWhereLike('description', "%{$request->search}%")
+            ->paginate()
+            ->withQueryString();
 
         return view('pages.courses.index', compact('courses'));
     }
--- resources/views/components/course-search.blade.php
+++ resources/views/components/course-search.blade.php
@@ -1,8 +1,10 @@
-<form>
+<form action="{{ route('courses.index') }}">
     <div class="flex gap-2">
         <div class="flex-grow">
             <input type="search"
+                   name="search"
                    placeholder="Course title or description"
+                   value="{{ request('search') }}"
                    class="w-full rounded-md border-gray-300 focus:border-violet-300 focus:ring focus:ring-violet-200 focus:ring-opacity-50">
         </div>
         <button class="rounded-md px-4 bg-violet-500 hover:bg-violet-600">
