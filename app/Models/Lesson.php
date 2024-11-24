87cc8f08cc53cb2c635da38ac45fd800e1a025d7
Igor Babko <i.i.babko@gmail.com>
2024-11-24 18:11:35 +0200

    50 - Accessors (previous / next)

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -13,5 +14,25 @@ class Lesson extends Model
     public function course(): BelongsTo
     {
         return $this->belongsTo(Course::class);
+    }
+
+    protected function previous(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this
+                ->course
+                ->lessons()
+                ->firstWhere('number', $this->number - 1),
+        );
+    }
+
+    protected function next(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this
+                ->course
+                ->lessons()
+                ->firstWhere('number', $this->number + 1),
+        );
     }
 }
--- resources/views/pages/lessons/show.blade.php
+++ resources/views/pages/lessons/show.blade.php
@@ -6,14 +6,18 @@
                 <x-lesson.player :$lesson/>
                 <div class="flex gap-2 flex-row">
                     <div class="flex gap-2 mr-auto">
-                        <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200 flex-grow"
-                           href="#">
-                            <x-icon name="chevron-left" class="size-6"/>
-                        </a>
-                        <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200 flex-grow"
-                           href="#">
-                            <x-icon name="chevron-right" class="size-6"/>
-                        </a>
+                        @if ($lesson->previous)
+                            <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200 flex-grow"
+                               href="{{ route('lessons.show', $lesson->previous) }}">
+                                <x-icon name="chevron-left" class="size-6"/>
+                            </a>
+                        @endif
+                        @if ($lesson->next)
+                            <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200 flex-grow"
+                               href="{{ route('lessons.show', $lesson->next) }}">
+                                <x-icon name="chevron-right" class="size-6"/>
+                            </a>
+                        @endif
                     </div>
                     <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200"
                        href="{{ route('courses.show', $lesson->course) }}">
