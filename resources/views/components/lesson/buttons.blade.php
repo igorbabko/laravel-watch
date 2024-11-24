1feea8cbea8a82792316f6b59f2abfba095d2e6d
Igor Babko <i.i.babko@gmail.com>
2024-11-24 19:56:24 +0200

    .......

--- resources/views/components/lesson/buttons.blade.php
+++ resources/views/components/lesson/buttons.blade.php
@@ -1,30 +1,27 @@
-<div class="flex gap-2 flex-row">
+<div class="flex gap-2">
     <div class="flex gap-2 mr-auto">
         @if ($lesson->previous)
-            <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200 flex-grow"
-               href="{{ $lesson->previous->routeUrl }}">
+            <a class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200" href="{{ $lesson->previous->routeUrl }}">
                 <x-icon name="chevron-left" class="size-6"/>
             </a>
         @endif
         @if ($lesson->next)
-            <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200 flex-grow"
-               href="{{ $lesson->next->routeUrl }}">
+            <a class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200" href="{{ $lesson->next->routeUrl }}">
                 <x-icon name="chevron-right" class="size-6"/>
             </a>
         @endif
     </div>
-    <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200"
+    <a class="rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200"
        href="{{ $lesson->course->routeUrl }}">
         All lessons
     </a>
-    <a class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200"
+    <a class="rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200"
        href="{{ $lesson->commit_url }}"
-       target="_blank"
-    >
+       target="_blank">
         Source Code
     </a>
     <button
-        class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200">
+        class="rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200">
         Mark as complete
     </button>
 </div>
