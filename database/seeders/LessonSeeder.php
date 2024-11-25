1a0717e6ecf1969a010037342414d2b0791f316b
Igor Babko <i.i.babko@gmail.com>
2024-11-25 06:43:23 +0200

    41. Lessons data

---
+++ database/seeders/LessonSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Lesson;
+use Illuminate\Database\Seeder;
+
+class LessonSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Lesson::truncate();
+
+        Lesson::factory(200)->create();
+    }
+}
