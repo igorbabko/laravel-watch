df6f4f0d890edff7b00df647f45b7a098c92f58f
Igor Babko <i.i.babko@gmail.com>
2024-11-25 08:12:13 +0200

    43. Has many relationship

--- resources/views/components/course/lessons.blade.php
+++ resources/views/components/course/lessons.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col gap-4 w-1/2">
-    <h3 class="text-lg font-semibold text-right">18 lessons &middot; 2h 25min</h3>
+    <h3 class="text-lg font-semibold text-right">{{ $lessons->count() }} lessons &middot; 2h 25min</h3>
     <div class="flex flex-col gap-4">
-        @foreach (App\Models\Lesson::take(20)->get() as $lesson)
+        @foreach ($lessons as $lesson)
             <a href="#" class="flex flex-col gap-2 p-4 rounded-md bg-gray-100 hover:bg-gray-200">
                 <div class="flex gap-2 justify-between font-mono">
                     <span class="text-sm">{{ $lesson->number }}</span>
