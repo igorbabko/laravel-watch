3819d575aed658a94786672e43a1737e282b0787
Igor Babko <i.i.babko@gmail.com>
2024-11-25 08:44:07 +0200

    45. Lesson page

--- resources/views/components/course/lessons.blade.php
+++ resources/views/components/course/lessons.blade.php
@@ -2,13 +2,7 @@
     <h3 class="text-lg font-semibold text-right">{{ $lessons->count() }} lessons &middot; 2h 25min</h3>
     <div class="flex flex-col gap-4">
         @foreach ($lessons as $lesson)
-            <a href="#" class="flex flex-col gap-2 p-4 rounded-md bg-gray-100 hover:bg-gray-200">
-                <div class="flex gap-2 justify-between font-mono">
-                    <span class="text-sm">{{ $lesson->number }}</span>
-                    <span class="text-sm text-gray-600">5m 32sec</span>
-                </div>
-                <h3 class="font-semibold">{{ $lesson->title }}</h3>
-            </a>
+            <x-lesson.card :$lesson/>
         @endforeach
     </div>
 </div>
