278511056cfa6f8d74b997368e3628e97097eb44
Igor Babko <i.i.babko@gmail.com>
2024-12-11 08:46:29 +0200

    Revert "."
    This reverts commit ff42df06dd723a744c2add05369671362f7e7b05.

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Tag;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -12,26 +11,10 @@ class CourseController extends Controller
 {
     public function index(Request $request): View
     {
-        dump($request->all());
-
         $courses = Course::latest()
             ->withSum('lessons as length', 'length')
-            ->when($request->search, fn (Builder $query) => $query
-                ->where(fn (Builder $query) => $query
-                    ->whereLike('title', "%{$request->search}%")
-                    ->orWhereLike('description', "%{$request->search}%")
-                )
-            )
-            ->when($request->tags, function (Builder $query) use ($request) {
-                collect($request->tags)
-                    ->each(fn ($tagId) => $query
-                        ->whereHas('tags', fn (Builder $query) => $query
-                            ->where('tags.id', $tagId)
-                        )
-                    );
-
-                return $query;
-            })
+            ->whereLike('title', "%{$request->search}%")
+            ->orWhereLike('description', "%{$request->search}%")
             ->paginate()
             ->withQueryString();
 
