05f58ffed0196fbc53138d29b557f80c03302b87
Igor Babko <i.i.babko@gmail.com>
2025-06-25 10:26:38 +0300

    59 - tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
             $this->call([
                 CourseSeeder::class,
                 LessonSeeder::class,
+                TagSeeder::class,
             ]);
         });
     }
