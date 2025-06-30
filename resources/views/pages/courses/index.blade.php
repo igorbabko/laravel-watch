e3085e5cdd479a3cd79dd6dec8210278f199da0e
Igor Babko <i.i.babko@gmail.com>
2025-06-30 06:16:44 +0300

    61. Filter form

--- resources/views/pages/courses/index.blade.php
+++ resources/views/pages/courses/index.blade.php
@@ -3,15 +3,32 @@
         <div class="container flex flex-col gap-8 pb-24">
             <h1 class="text-center font-extrabold text-5xl">Courses</h1>
             <div class="flex items-start gap-8">
-                <div class="sticky top-24 w-1/5">
-                    <x-course.filter :$tags/>
+                <div class="sticky top-24 w-1/4">
+                    <form action="{{ route('courses.index') }}" class="flex flex-col gap-6">
+                        <x-course.search/>
+                        <x-course.filter :$tags/>
+                        <div class="flex gap-2">
+                            <a href="{{ route('courses.index') }}"
+                               class="whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200 grow text-center">
+                                Reset
+                            </a>
+                            <button
+                                class="whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-violet-500 text-white hover:bg-violet-600 grow text-center cursor-pointer">
+                                Search
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <div class="flex flex-col gap-4 w-4/5">
-                    <x-course.search/>
+                <div class="flex flex-col gap-4 w-3/4">
                     <div class="grid grid-cols-2 gap-4">
-                        @foreach ($courses as $course)
+                        @forelse ($courses as $course)
                             <x-course.card :$course/>
-                        @endforeach
+                        @empty
+                            <p>
+                                No courses found, <a href="{{ route('courses.index') }}"
+                                                     class="font-semibold text-violet-600">reset</a>
+                            </p>
+                        @endforelse
                     </div>
                     {{ $courses->links() }}
                 </div>
