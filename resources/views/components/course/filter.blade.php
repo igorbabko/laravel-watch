56ec930881c9a064ce15a96f7aaab0f25bcdeadb
Igor Babko <i.i.babko@gmail.com>
2025-07-07 11:41:42 +0300

    62. Filtering

--- resources/views/components/course/filter.blade.php
+++ resources/views/components/course/filter.blade.php
@@ -3,6 +3,9 @@
         <li>
             <label class="font-medium flex items-center gap-3">
                 <input type="checkbox"
+                       name="tags[]"
+                       value="{{ $tag->id }}"
+                       {{ in_array($tag->id, request('tags', [])) ? 'checked' : '' }}
                        class="size-5 rounded-md border-gray-300 text-violet-600 focus:border-violet-300 focus:ring focus:ring-violet-200 focus:ring-opacity-50 focus:ring-offset-0">
                 {{ $tag->name }}
             </label>
