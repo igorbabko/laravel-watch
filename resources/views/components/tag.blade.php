ff2c1b398e5fa161c73d08adea77ed3cff936866
Igor Babko <i.i.babko@gmail.com>
2025-07-15 07:12:01 +0300

    65. Topics

---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,13 @@
+<a href="{{ route('courses.index', ['tags' => [$tag->id]]) }}" class="rounded-md bg-gray-100 p-4 hover:bg-gray-200">
+    <div class="flex flex-col justify-center items-center gap-4">
+        <div class="flex justify-center items-center gap-2">
+            <img src="/img/{{ $tag->name }}.svg" alt="{{ $tag->name }}" class="size-12">
+            <div class="flex flex-col gap-2">
+                <h3 class="font-semibold text-xl">{{ $tag->name }}</h3>
+            </div>
+        </div>
+        <p class="text-center font-medium">
+            4 courses <br> 120 lessons
+        </p>
+    </div>
+</a>
