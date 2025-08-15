34852d2ef63bcffcfc584ec20ddc04c3b6dcce63
Igor Babko <i.i.babko@gmail.com>
2025-08-15 08:51:45 +0300

    customize

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,8 +23,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-//        DB::listen(function (QueryExecuted $query) {
-//            dump([$query->sql, $query->bindings, $query->time, $query->toRawSql()]);
-//        });
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Verify Email Address')
+                ->line('Click the button below to verify your email address.')
+                ->action('Verify!', $url);
+        });
+
+        //        DB::listen(function (QueryExecuted $query) {
+        //            dump([$query->sql, $query->bindings, $query->time, $query->toRawSql()]);
+        //        });
     }
 }
