1d647d826976a268b34d435c1765fc5c5ec28262
Igor Babko <i.i.babko@gmail.com>
2025-08-15 09:03:36 +0300

    Revert "customize"
    This reverts commit 34852d2ef63bcffcfc584ec20ddc04c3b6dcce63.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Database\Events\QueryExecuted;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,15 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
-            return (new MailMessage)
-                ->subject('Verify Email Address')
-                ->line('Click the button below to verify your email address.')
-                ->action('Verify!', $url);
-        });
-
-        //        DB::listen(function (QueryExecuted $query) {
-        //            dump([$query->sql, $query->bindings, $query->time, $query->toRawSql()]);
-        //        });
+//        DB::listen(function (QueryExecuted $query) {
+//            dump([$query->sql, $query->bindings, $query->time, $query->toRawSql()]);
+//        });
     }
 }
