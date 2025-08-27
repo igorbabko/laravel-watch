54824b2c92d5b3ffd1cd3c29a36ad78fd5f4edc5
Igor Babko <i.i.babko@gmail.com>
2025-08-27 09:33:23 +0300

    81. Account deletion

--- resources/views/pages/delete-account.blade.php
+++ resources/views/pages/delete-account.blade.php
@@ -4,24 +4,28 @@
             <h1 class="text-center font-extrabold text-5xl">Delete Account</h1>
             <div class="flex gap-3 bg-red-50 border border-red-200 rounded-md p-4">
                 <div class="flex-shrink-0">
-                    <x-icon name="exclamation-triangle" class="size-6 text-red-800" />
+                    <x-icon name="exclamation-triangle" class="size-6 text-red-800"/>
                 </div>
                 <div>
                     <h2 class="text-sm font-medium text-red-800">
                         Warning: This action cannot be undone
                     </h2>
-                    <p class="mt-2 text-sm text-red-700">Once you delete your account, all of your data will be permanently removed.</p>
+                    <p class="mt-2 text-sm text-red-700">Once you delete your account, all of your data will be
+                        permanently removed.</p>
                 </div>
             </div>
-            <form action="#" method="POST" class="flex flex-col gap-4">
+            <form action="{{ route('delete-account.destroy') }}" method="POST" class="flex flex-col gap-4">
                 @csrf
+                @method('DELETE')
                 <div class="flex items-center gap-2 justify-between">
                     <a href="{{ route('profile') }}"
                        class="flex justify-center items-center gap-2 whitespace-nowrap rounded-md font-semibold px-4 py-2 bg-gray-100 hover:bg-gray-200">
-                       Cancel
+                        Cancel
                     </a>
                     <button type="submit"
-                            class="flex justify-center items-center gap-2 whitespace-nowrap cursor-pointer rounded-md font-semibold px-4 py-2 bg-red-600 text-white hover:bg-red-700">
+                            class="flex justify-center items-center gap-2 whitespace-nowrap cursor-pointer rounded-md font-semibold px-4 py-2 bg-red-600 text-white hover:bg-red-700"
+                            onclick="return confirm('Are you absolutely sure you want to delete your account? This action cannot be undone.')"
+                    >
                         Delete
                     </button>
                 </div>
